809846a9c0a328efec1bcc64454eb90883bc187b
MaxLavrinenko <79162671+MaxLavrinenko@users.noreply.github.com>
2022-02-17 17:53:09 +0200

    hw1

--- CW.py
+++ CW.py
@@ -1,5 +1,50 @@
 # створити пустый список users_list = []
 #
+users_list = []
+while True:
+    print('1) додававання нового юзера')
+    print('2) вивід всіх юзерів')
+    print('3) вивід всіх юзерів за віком')
+    print('4) видалення юзера по id')
+    print('5) заміна статуса юзера на протилежний')
+    print('6) вихід з меню')
+
+    choise = int(input('Введите пункт меню : '))
+
+    if choise not in [1, 2, 3, 4, 5, 6]:
+        print('Нет такого пункта')
+        continue
+    if choise == 1:
+        id = input('Введите ID : ')
+        name = input('Имя юзера : ')
+        age = input('Возраст юзера : ')
+        status = input('Статус юзера : ')
+
+        users_list.append({'id': id, 'name': name, 'age': age, 'status': status})
+        continue
+    elif choise == 2:
+        print(users_list)
+        continue
+    elif choise == 3:
+
+        continue
+    elif choise == 4:
+        id = int(input('id= '))
+        for i in users_list:
+            print(i)
+            if i['id'] == 'id':
+                print(i['name'])
+        continue
+    elif choise == 5:
+        for i in users_list:
+            if i['status'] == 'True':
+                i['status'] = 'False'
+            elif i['status'] == 'False':
+                i['status'] = 'True'
+        print(users_list)
+        continue
+    elif choise == 6:
+        break
 # стврорити меню в якому потрібно реалізувати:
 #
 # 1) додававання нового юзера
--- HW.py
+++ HW.py
@@ -44,6 +44,7 @@
 #
 # greeting = 'Hello, world'
 # l=[]
+
 # for i in greeting:
 #     l.append(i.upper())
 #
@@ -53,27 +54,162 @@
 # пример:
 # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, ...]
 #
+l = [i ** 2 for i in range(50) if i % 2 != 0]
+
+# print(l)
+
 # function
 #
 # - створити функцію яка виводить ліст
+# def listWriter(list):
+#     print(list)
+#
+# listWriter(l)
+
 # - створити функцію яка приймає три числа та виводить та повертає найменьше.
+# def minFunc (a,b,c):
+#     l=[a,b,c]
+#     char = min(l)
+#     print(char)
+#     return char
+
+
+# minNum = minFunc(14,82,63)
+# print(minNum)
+
 # - створити функцію яка приймає три числа та виводить та повертає найбільше.
+# def maxFunc(a, b, c):
+#     l = [a, b, c]
+#     char = max(l)
+#     print(char)
+#     return char
+#
+#
+# maxNum = maxFunc(14, 82, 63)
+# print(maxNum)
+
 # - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше
+# def someFunc(*args):
+#     print(max(args))
+#     return min(args)
+# minNum = someFunc(1,2,3,34,54,0.5)
+# print(minNum)
+
 # - створити функцію яка виводить ліст
+# def someFunc(*args):
+#      print(args)
+#
+# someFunc(l)
+
+
 # - створити функцію яка повертає найбільше число з ліста
+# def someFunc(args):
+#     maxChar = max(args)
+#     return maxChar
+#
+#
+# maxNum = someFunc(l)
+# print(maxNum)
+
+
 # - створити функцію яка повертає найменьше число з ліста
+# def someFunc(args):
+#     minChar = min(args)
+#     return minChar
+#
+# minNum = someFunc(l)
+# print(minNum)
+
+
 # - створити функцію яка приймає ліст чисел та складає значення елементів ліста та повертає його.
-# - створити функцію яка приймає ліст чисел та повертає середнє арифметичне його значень.
+# def someFunc(args):
+#     sum = 0
+#     for i in args:
+#         sum = sum + i
+#     return sum
+#
+#
+# sumNum = someFunc(l)
+# print(sumNum)
+
+# # - створити функцію яка приймає ліст чисел та повертає середнє арифметичне його значень.
+# def someFunc(args):
+#     sum = 0
+#     for i in args:
+#         sum = sum + i
+#     return sum / len(args)
+#
+#
+# char = someFunc(l)
+# print(char)
 #
 # #################################################################################################
 # 1)Дан лист:
-#   list = [22, 3,5,2,8,2,-23, 8,23,5]
+# list = [22, 3, 5, 2, 8, 2, -23, 8, 23, 5]
 #   - найти min число в листе
+# minChar = min(list)
+# print(minChar)
+
 #   - удалить все дубликаты в листе
+# newList = [i for i in list if list.count(i) <= 1]
+# print(newList)
+
 #   - заменить каждое четвертое значение на "Х"
+# for i in list[4::4]:
+#     list[list.index(i)] = 'X'
+# print(list)
+
 # 2)вывести на экран пустой квадрат из "*" сторона которого указана в переменой:
+# side = 8
+# i = side
+# while i >= 1:
+#     if i == side or i == 1:
+#         j = side
+#         while j > 0:
+#                 print('*', end='')
+#                 j -= 1
+#         print('')
+#     else:
+#         j = side - 2
+#         print('*', end='')
+#         while j > 0:
+#             print(' ', end='')
+#             j -= 1
+#         print('*')
+#     i -= 1
+
 # 3) вывести табличку умножения с помощью цикла while
+
+# i = 1
+# while i <= 9:
+#     j = 1
+#     while j <= 9:
+#         print(i * j, end='\t')
+#         j = j + 1
+#     print('')
+#     i = i +1
 # 4) переделать первое задание под меню с помощью цикла
+# list = [22, 3, 5, 2, 8, 2, -23, 8, 23, 5]
+# while True:
+#     print("1. Найти min число в листе")
+#     print("2. Удалить все дубликаты в листе")
+#     print("3. Заменить каждое четвертое значение на 'X' ")
+#
+#     choise = int(input("Введите № задания от 1 до 3: "))
+#     if choise == 1:
+#         print("1.Найти min число в листе")
+#         print(min(list))
+#     elif choise == 2:
+#         newList = [i for i in list if list.count(i) <= 1]
+#         print(newList)
+#     elif choise == 3:
+#         print("3. Заменить каждое четвертое значение на 'X'" )
+#         for i in list[4::4]:
+#             list[list.index(i)] = 'X'
+#         print(list)
+#     else:
+#         print("Нет такого пункта")
+#     break
 #
 # ***  - вывести элемент листа, значение которого ближе всего к среднему арифметическому всех элементов этого же листа
 # пример:
