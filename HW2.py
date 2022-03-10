4b6bb40e1bd6cd08b33ef05473d7a4c9dbb6a148
MaxLavrinenko <79162671+MaxLavrinenko@users.noreply.github.com>
2022-03-10 12:37:42 +0200

    hw2

--- HW2.py
+++ HW2.py
@@ -4,27 +4,27 @@
 #
 # запишите 5 тудушек
 # и выведете все
-def notebook():
-    todoList: list[str] = []
-
-    def addTodo(new: str) -> None:
-        nonlocal todoList
-        todoList.append(new)
-
-    def readTodo() -> list[str]:
-        print(todoList)
-        return todoList
-
-    return addTodo, readTodo
-
-
-addTodo, readTodo = notebook()
-addTodo('wake up')
-addTodo('eat')
-addTodo('ddos russians')
-addTodo('read news')
-addTodo('sleep')
-readTodo()
+# def notebook():
+#     todoList: list[str] = []
+#
+#     def addTodo(new: str) -> None:
+#         nonlocal todoList
+#         todoList.append(new)
+#
+#     def readTodo() -> list[str]:
+#         print(todoList)
+#         return todoList
+#
+#     return addTodo, readTodo
+#
+#
+# addTodo, readTodo = notebook()
+# addTodo('wake up')
+# addTodo('eat')
+# addTodo('ddos russians')
+# addTodo('read news')
+# addTodo('sleep')
+# readTodo()
 
 ########
 
@@ -32,6 +32,17 @@ readTodo()
 
 # 3) создать функцию которая будет возвращать сумму разрядов числа в виде строки (тоже с типизацией)
 #
+
+def expanded(num: str) -> None:
+    n = str(num)
+    n1 = n[len(n) - 1]
+    for i in num:
+        i = str(i)
+        print(i)
+
+
+expanded('1234')
+
 # Пример:
 #
 # expanded_form(12) # return '10 + 2'
