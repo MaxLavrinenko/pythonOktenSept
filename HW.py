500d368785e866e59d2e8e75bd43459e8eeeff80
MaxLavrinenko <79162671+MaxLavrinenko@users.noreply.github.com>
2022-03-23 13:03:01 +0200

    hw4

--- HW.py
+++ HW.py
@@ -1,6 +1,9 @@
 try:
-    with open('emails.txt','r') as file:
-        for i in file:
-
+    with open('emails.txt', 'r') as infile:
+        with open('gmail.txt', 'a') as outfile:
+            for i in infile:
+                i: str = i
+                if i.endswith('@gmail.com\n'):
+                    outfile.write(i.split()[-1] + '\n')
 except Exception as err:
     print(err)
